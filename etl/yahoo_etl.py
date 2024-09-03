6fe513acfe923fa1dd2eb18d2e33731140103558
Peter <j100782002@gmail.com>
2024-09-03 14:21:47 +0800

    add yahoo etl

---
+++ etl/yahoo_etl.py
@@ -0,0 +1,11 @@
+from .base_etl import BaseETL
+
+class ShopeeETL(BaseETL):
+    def extract(self):
+        # 從yahoo提取商品數據
+        pass
+    
+    def transform(self, data):
+        # 轉換yahoo的數據格式，提取所需的商品信息
+        pass
+    
