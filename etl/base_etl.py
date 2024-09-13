58046e757ebf4bb5d173d6b3ceb18d1878a2e7d6
Peter <j100782002@gmail.com>
2024-09-13 12:05:15 +0800

    adjust init attribute to product and transform to URL form, add upload_to_sheet util to load method

--- etl/base_etl.py
+++ etl/base_etl.py
@@ -1,15 +1,18 @@
 from abc import ABC, abstractmethod
-
+from utils.google_sheets import upload_to_sheets
+import urllib.parse  
 
 class BaseETL(ABC):
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, prod_name):
+        self.prod_name = prod_name
+        # 將產品名稱轉換成url編碼，方便直接帶入網址
+        self.encoded_prod_name = urllib.parse.quote(prod_name)
     
     def process(self):
         # 執行整個 ETL 流程
         data = self.extract()
         transformed_data = self.transform(data)
-        self.load(transformed_data)
+        self.load(transformed_data, self.prod_name)
     
     @abstractmethod
     def extract(self):
@@ -21,6 +24,6 @@ class BaseETL(ABC):
         # 轉換和清理提取的數據
         pass
     
-    def load(self, data):
+    def load(self, data, prod_name):
         # 將轉換後的數據加載到 Google Sheets
-        pass
+        upload_to_sheets(data, prod_name)
